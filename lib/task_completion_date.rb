1972f2e99974a4f58e4eaf45538239cc454b0ea5
Sebastjan Hribar <sebastjan.hribar@gmail.com>
2015-10-21 20:33:45 +0200

    readme

--- lib/task_completion_date.rb
+++ lib/task_completion_date.rb
@@ -1,7 +1,6 @@
 require_relative "task_completion_date/version"
 require 'date'
 
-
 def calculate_task_completion(start_date, task_start_time, work_end_time, workload, work_hours_in_day, holidays)
   time_left_first_day = work_end_time - task_start_time
   no_of_days_for_task = (workload - time_left_first_day) / work_hours_in_day
@@ -31,5 +30,5 @@ def calculate_task_completion(start_date, task_start_time, work_end_time, worklo
 
   final_day = target_date1 + 1
   final_hour = 9 + portion
+  puts "Task completion date and time are: #{final_day}, #{final_hour}."
 end
-calculate_task_completion(Date.new(2015,10,20), 9, 17, 80, 7.5, [Date.new(2015,10,22), Date.new(2015,10,25)])
